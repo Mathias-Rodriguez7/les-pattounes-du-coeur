6d1257c190ba50e730e1ded4f2760f3698625764
Mathias Rodriguez <zoxor7@gmal.com>
2026-04-14 09:42:53 +0200

    #15 layout Footer setup

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -2,8 +2,8 @@
 	import '../../../app.css';
 </script>
 
-<footer class="flex gap-6 bg-accent justify-around p-2">
-    <img src="/img/logo.png" alt="logo" class=" h-30">
+<footer class="flex bg-accent justify-around items-center">
+    <img src="/img/logo.png" alt="logo" class=" h-20">
     <nav>
         <ul>
             <li><a href="##" aria-label="link">Accueil</a></li>
@@ -19,7 +19,7 @@
             <li><a href="##" aria-label="link">signaler un chat airant</a></li>
         </ul>
     </nav>
-    <a href="https://www.helloasso.com/associations/les-pattounes-du-coeur/formulaires/1" target="_blank" rel="noopener noreferrer"><img src="/img/cat$.jpg" alt="cat" class=" h-30 border rounded-4xl"></a>
+    <a href="https://www.helloasso.com/associations/les-pattounes-du-coeur/formulaires/1" target="_blank" rel="noopener noreferrer"><img src="/img/cat$.jpg" alt="cat" class=" h-20 border rounded-4xl"></a>
     <a href="https://www.helloasso.com/associations/les-pattounes-du-coeur/formulaires/1" target="_blank" rel="noopener noreferrer"><img src="/img/youtube.png" alt="youtube" class=" w-30"></a>
     <a href="https://www.helloasso.com/associations/les-pattounes-du-coeur/formulaires/1" target="_blank" rel="noopener noreferrer"><img src="/img/instagram.png" alt="insta" class=" w-20"></a>
     <a href="https://www.helloasso.com/associations/les-pattounes-du-coeur/formulaires/1" target="_blank" rel="noopener noreferrer"><img src="/img/facebook.png" alt="fb" class=" w-13"></a>
