910f5415b39b45dff984bdb23f244e3aff527b95
Mathias Rodriguez <zoxor7@gmal.com>
2026-05-28 15:55:58 +0200

    fix

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,7 +1,8 @@
 import { PrismaClient } from '../../generated/prisma/client.js';
-import { DATABASE_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import { PrismaPg } from '@prisma/adapter-pg';
 
+const { DATABASE_URL } = env;
 const adapter = new PrismaPg({
 	connectionString: DATABASE_URL
 });
